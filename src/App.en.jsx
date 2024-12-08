19a1ea4db77a0aaa4ddfc2449361b26078826d43
lyjmry <45060067+lyjmry@users.noreply.github.com>
2024-12-08 15:31:52 +0800

    Add language switch and update routing

--- src/App.en.jsx
+++ src/App.en.jsx
@@ -1,34 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout/index.en';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard/index.en';
-import Management from './pages/Management/index.en';
-import NFTMarket from './pages/NFTMarket/index.en';
-import UserManagement from './pages/Management/User/index.en';
-import DeviceManagement from './pages/Management/Device/index.en';
-import AdvertiserManagement from './pages/Management/Advertiser/index.en';
-import DeveloperManagement from './pages/Management/Developer/index.en';
-import ReviewerManagement from './pages/Management/Reviewer/index.en';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router.en';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="nft-market" element={<NFTMarket />} />
-          <Route path="management" element={<Management />}>
-            <Route path="user" element={<UserManagement />} />
-            <Route path="device" element={<DeviceManagement />} />
-            <Route path="advertiser" element={<AdvertiserManagement />} />
-            <Route path="developer" element={<DeveloperManagement />} />
-            <Route path="reviewer" element={<ReviewerManagement />} />
-          </Route>
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
