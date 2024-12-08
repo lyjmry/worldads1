9fa24349734e114437662364ccaba283a3e7d023
lyjmry <45060067+lyjmry@users.noreply.github.com>
2024-12-08 15:41:38 +0800

    Add language switch functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import AppEn from './App.en.jsx'
+import { LanguageProvider, useLanguage } from './contexts/LanguageContext'
+import './index.css'
+
+function Root() {
+  const { language } = useLanguage();
+  return language === 'zh' ? <App /> : <AppEn />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <LanguageProvider>
+      <Root />
+    </LanguageProvider>
+  </React.StrictMode>,
+)
