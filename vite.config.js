3c91857901015f30f89f4b9071b7874c922ff2f5
lyjmry <45060067+lyjmry@users.noreply.github.com>
2024-12-08 15:17:57 +0800

    Add English version of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 允许局域网访问
-    port: 3000, // 使用3000端口
-    strictPort: true, // 端口被占用时不自动尝试下一个端口
-    open: false, // 不自动打开浏览器
+    port: 3001,
+    open: '/en.html' // 默认打开英文版
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        en: './en.html'
+      }
+    }
   }
-});
+})
